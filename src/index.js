8aa48ee1a5698fae9a1c2dec5e851a234a3a3dad
MedKhalilLabidi <mohamedkhalil.labidi@esprit.tn>
2022-08-16 20:27:33 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import App from './app/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./app/App";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./app/store";
 import "./i18n";
-import * as serviceWorker from './serviceWorker';
-
+import * as serviceWorker from "./serviceWorker";
 ReactDOM.render(
-  <BrowserRouter basename="/demo/corona-react-free/template/demo_1/preview">
-    <App />
-  </BrowserRouter>
-, document.getElementById('root'));
-
+  <Provider store={store}>
+    <BrowserRouter basename="/demo/corona-react-free/template/demo_1/preview">
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
+);
 serviceWorker.unregister();
