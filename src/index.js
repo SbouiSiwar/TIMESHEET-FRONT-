e963a687e4c596f8ef02c922d5a9704579d6d6e9
MedKhalilLabidi <mohamedkhalil.labidi@esprit.tn>
2022-08-16 19:51:36 +0200

    Revert "push"
    This reverts commit 2609bf8ea3b163c25973cae67b35e9b667e659aa.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./app/App";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "./app/store";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import App from './app/App';
 import "./i18n";
-import * as serviceWorker from "./serviceWorker";
+import * as serviceWorker from './serviceWorker';
+
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter basename="/demo/corona-react-free/template/demo_1/preview">
-      <App />
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById("root")
-);
+  <BrowserRouter basename="/demo/corona-react-free/template/demo_1/preview">
+    <App />
+  </BrowserRouter>
+, document.getElementById('root'));
+
 serviceWorker.unregister();
